eb706e845a94a0a5c049e751d064c5f8f7e5f5fd
fandi a <fandiiardy@gmail.com>
2024-11-17 16:08:12 +0700

    Add Cards

--- src/Components/NewsLetter.jsx
+++ src/Components/NewsLetter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NewsLetter = () => {
     return (
         <div className='w-full py-16 text-white px-4'>
-            <div className='mx-w-[1240px] mx-auto grid lg:grid-cols-3'>
+            <div className='mx-w-[1240px] mx-auto grid lg:grid-cols-3 px-20'>
                 <div className='lg:col-span-2 my-4'>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want Tips & Tricks?</h1>
                     <p>Sign up to our newsletter and stay up to date.</p>
